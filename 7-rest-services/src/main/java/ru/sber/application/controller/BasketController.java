d42604f2f34666e1555a9992f1e54559ee88ee7a
lepeshichev <lepeshichev>
2023-06-28 12:56:41 +0300

    Обновлен rest-модуль

---
+++ 7-rest-services/src/main/java/ru/sber/application/controller/BasketController.java
@@ -0,0 +1,40 @@
+package ru.sber.application.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.sber.application.entity.Product;
+import ru.sber.application.repository.BasketRepository;
+
+@Slf4j
+@RequestMapping("basket")
+@RestController
+public class BasketController {
+    private BasketRepository basketRepository;
+
+    public BasketController(BasketRepository basketRepository) {
+        this.basketRepository = basketRepository;
+    }
+
+    @PostMapping
+    public boolean add(@RequestBody Product product) {
+        log.info("Добавление в корзину {}", product);
+        return basketRepository.add(product);
+    }
+
+    @PutMapping
+    public boolean updateBasket(@RequestBody Product product) {
+        log.info("Изменение количества продукта в корзине");
+        return basketRepository.update(10, 1);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        boolean isDeleted = basketRepository.deleteById(id);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
--- 7-rest-services/src/main/java/ru/sber/application/repository/BasketRepository.java
+++ 7-rest-services/src/main/java/ru/sber/application/repository/BasketRepository.java
@@ -4,6 +4,6 @@ import ru.sber.application.entity.Product;
 
 public interface BasketRepository {
     boolean add(Product product);
-    boolean update(int amount);
+    boolean update(int amount, long id);
     boolean deleteById(long id);
 }
